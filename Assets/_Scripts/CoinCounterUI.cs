4d2c14e97afb72db63909955a896b4898b9d79c2
AmanKaushik77 <100389449+AmanKaushik77@users.noreply.github.com>
2025-03-12 00:36:05 -0700

    Coin animation pt 1

---
+++ Assets/_Scripts/CoinCounterUI.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+
+public class CoinCounterUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI current;
+    [SerializeField] private TextMeshProUGUI toUpdate;
+    [SerializeField] private Transform coinTextContainer;
+    [SerializeField] private float duration;
+
+    private float containerInitPosition;
+    private float moveAmount;
+
+    private void Start()
+    {
+        Canvas.ForceUpdateCanvases();
+        current.SetText("0");
+        toUpdate.SetText("0");
+        containerInitPosition = coinTextContainer.localPosition.y;
+        moveAmount = current.rectTransform.rect.height;
+    }
+
+    public void UpdateScore(int score)
+    {
+        // Set the score to the masked text UI
+        toUpdate.SetText($"{score}");
+
+        // Trigger the local move animation
+        coinTextContainer.DOLocalMoveY(containerInitPosition + moveAmount, duration);
+    }
+
+}
